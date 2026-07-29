df78def961f66753aeacb6599792b6b0922ccfaf
amyhong0 <heywon16@gmail.com>
2026-07-29 17:29:36 +0900

    chore: cleanup temporary test and debug files

--- scratch/print_raw_snippet.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-const html = fs.readFileSync('scratch/ig_page.html', 'utf8');
-
-const targetIndex = 567731;
-const snippet = html.substring(targetIndex, targetIndex + 3000);
-console.log(snippet);
